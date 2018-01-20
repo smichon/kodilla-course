ca7472a0066ed35a70813b9572afd3471e4700c5
smichon <sylwester.michon@gmail.com>
2018-01-20 23:03:44 +0100

    mod 7.2 immutable class ForumUser added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -15,6 +16,10 @@ public class StreamMain {
         poemBeautifier.beautify(PoemBeautifier::replaceELetterWith3);
         poemBeautifier.beautify(() -> System.out.println("\nIn this text one letter is replaced by number"
                 .replace("t", "1")));
-        poemBeautifier.beautify(() -> System.out.println("\nText to hide".replaceAll(".","*")));
+        poemBeautifier.beautify(() -> System.out.println("\nText to hide".replaceAll(".","*")
+                +"\nText above has been substituted by asterisks\n_________________________"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
