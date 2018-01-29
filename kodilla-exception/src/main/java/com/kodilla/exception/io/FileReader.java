c713cfe0228280f7e061e897b89a5fe3a9fd73c7
smichon <sylwester.michon@gmail.com>
2018-01-29 21:47:20 +0100

    mod 8.3 exceptions  added

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
@@ -16,7 +16,8 @@ public class FileReader {
             fileLines.forEach(System.out::println);
 
         } catch (IOException e) {
-            System.out.println("Oh no! Something went wrong!" + e);
+            throw new FileReaderException();
+
         } finally {
             System.out.println("I am gonna be here ... always!");
         }
