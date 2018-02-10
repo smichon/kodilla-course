fd38bbb5db10d82a4dc95dedaadd6f593f00400e
smichon <sylwester.michon@gmail.com>
2018-02-10 13:21:13 +0100

    mod 10.1 testForumUser added

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class ForumUserTestSuite {
+
+    @Test
+    public void testGetUsername() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String name = forumUser.username;
+        //Then
+        Assert.assertEquals("John Smith", name);
+    }
+}
